8b7d3b6f0e095d4b0ad8a1d59b2bd37d4e932ed1
Ethan Zhang <zhangyuzhi1993@gmail.com>
2022-08-06 00:11:04 +0800

    chore(icons): use unplugin-icons

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn2.jianshu.io'],
+    domains: ['cdn2.jianshu.io', "upload-images.jianshu.io"],
   },
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.plugins.push(
+      require('unplugin-icons/webpack')({
+        compiler: 'jsx',
+        jsx: 'react',
+      })
+    )
+
+    return config
+  }
 }
 
 module.exports = nextConfig
